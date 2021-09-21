ed325eb04f328a64021746b773b1cd31e64bf8f7
jagdishjadeja <jagdishsinhisjadeja@gmail.com>
2021-09-21 23:38:53 +0530

    push

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot(
+      {
+        rootPath: join(__dirname, '..', 'serve-multi-spas-demo'),
+      },
+      {
+        rootPath: join(__dirname, '..', 'my-application'),
+        serveRoot: 'app1',
+      },
+      {
+        rootPath: join(__dirname, '..', 'my-application2'),
+        serveRoot: 'app2',
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
